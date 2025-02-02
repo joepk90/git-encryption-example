79a0eb0ec230a4e58ab0da64be4ca24bfa4e7c12
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-02-02 22:29:03 +0000

    update the error message in the check-all.sh script

--- bash/check-all.sh
+++ bash/check-all.sh
@@ -9,7 +9,8 @@ for file in $(find ./ -type f -path "*.$SECRETS_EXT"); do
     IS_ENCRYPTED=$(openssl aes-256-cbc -d -a -nosalt -pbkdf2 -pass pass:$ENCRYPTION_SEED -in "$file" -out /dev/null 2>/dev/null && echo "true" || echo "false")
 
     if [ "$IS_ENCRYPTED" = "false" ]; then
-        echo "Some files are not encrypted or the decryption failed. The first files found was: $file"
+        echo "ERROR: Some staged files are not encrypted or the decryption failed."
+        echo "The first files found was: $file"
         exit 1
     fi
 
