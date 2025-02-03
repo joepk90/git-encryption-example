b366e75a15bda5bcdd2a367b43d2c3dab3380317
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-02-03 20:12:37 +0000

    update check-staged bash script to use file_is_secret util function

--- bash/check-staged.sh
+++ bash/check-staged.sh
@@ -11,14 +11,15 @@ STAGED_FILES=$(git diff --cached --name-only)
 
 for file in $STAGED_FILES; do
 
+    IS_SECRET_FILE=$(file_is_secret $file)
+
     # if file path does not include the secrets extension, continue to the next item
-    if [[ "$file" != *"$SECRETS_EXT"* ]]; then
+    if [ "$IS_SECRET_FILE" = "false" ]; then
         continue
     fi
 
     # if ile path does include the secrets extension, check if the file has been encrypted
     IS_ENCRYPTED=$(is_file_encrypted $file)
-
 
     if [ "$IS_ENCRYPTED" = "false" ]; then
         echo "ERROR: Some staged files are not encrypted or the decryption failed."
